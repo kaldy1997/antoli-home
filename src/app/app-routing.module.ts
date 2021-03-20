1008c4e285dd362d43e4b4dd8acaef03835344cb
Claudio <claudiosfero@gmail.com>
2021-03-20 10:48:42 +0100

    Construcción V1
    Todos los textos e imágenes introducidas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './functionalities/contacto/contacto.component';
+import { GaleriaComponent } from './functionalities/galeria/galeria.component';
+import { HomeComponent } from './functionalities/home/home.component';
+import { QuienesSomosComponent } from './functionalities/quienes-somos/quienes-somos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'quienes-somos', component: QuienesSomosComponent
+  }
+  // {
+  //   path: 'galeria', component: GaleriaComponent
+  // },
+  // {
+  //   path: 'contacto', component: ContactoComponent
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ContactoComponent } from './functionalities/contacto/contacto.component';
+import { GaleriaComponent } from './functionalities/galeria/galeria.component';
+import { HomeComponent } from './functionalities/home/home.component';
+import { QuienesSomosComponent } from './functionalities/quienes-somos/quienes-somos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    QuienesSomosComponent,
+    GaleriaComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
