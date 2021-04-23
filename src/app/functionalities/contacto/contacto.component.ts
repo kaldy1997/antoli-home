eea39778554ac394b5d2bf60b60925dd2d0b28c4
Claudio <claudiosfero@gmail.com>
2021-04-23 18:37:22 +0200

    add maps button

--- src/app/functionalities/contacto/contacto.component.ts
+++ src/app/functionalities/contacto/contacto.component.ts
@@ -21,6 +21,10 @@ export class ContactoComponent implements OnInit {
     window.open('http://www.costablanca.org/Esp/Descubre_la_Costa_Blanca/La_Costa_Blanca/Paginas/Agres.aspx', '_blank');
   }
 
+  public openMap(): void {
+    window.open('https://www.google.es/maps/place/Casa+Rural+Antol%C3%AD/@38.7802115,-0.5169386,18z/data=!4m13!1m7!3m6!1s0xd61848640e61b75:0xe5d3f0e51621975f!2s03837+Agres,+Alicante!3b1!8m2!3d38.7808327!4d-0.5157406!3m4!1s0x0:0x609d57bbb09a5c71!8m2!3d38.78058!4d-0.51739', '_blank');
+  }
+
   private setMap(): void {
     Mapboxgl.accessToken = environment.mapboxKey;
     this.mapa = new Mapboxgl.Map({
@@ -32,7 +36,5 @@ export class ContactoComponent implements OnInit {
 
     new Mapboxgl.Marker().setLngLat(this.COORDENADAS).addTo(this.mapa);
     this.mapa.addControl(new Mapboxgl.NavigationControl());
-    this.mapa.addControl(new Mapboxgl.MapboxGeocoder(), 'top-left');
   }
-
 }
